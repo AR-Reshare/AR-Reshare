2f7c029add8de8c0367bd3cf4997be68e2cc20a8
Joe Shepherd <sudo.jshepherd01@gmail.com>
2021-12-09 17:03:50 +0000

    Refactor (probably the first of many)

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-/* This file will contain the main code of the server. It will connect endpoints to pipeline systems,
- * include specific code for miscellaneous paths, and unpack data from requests
+/* this should just start the app running and listening to a port
 */
