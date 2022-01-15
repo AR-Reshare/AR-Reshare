c9a7e89001bda1218208e4df92a844f4e447449b
Joe Shepherd <sudo.jshepherd01@gmail.com>
2022-01-15 21:01:26 +0000

    Create executeQuery function

--- database-funcs.js
+++ database-funcs.js
@@ -32,11 +32,59 @@ const executeCommand = (queryTemplate, values=[]) => {
     });
 }
 
+/**
+ * Executes a query on the database. Expects something to be returned.
+ * @async
+ * @param {string} queryTemplate - The query to execute, which may be parameterised 
+ * @param {string[]} [values=[]] - (Optional, Array) The values to substitute into the parameterised query
+ * @returns {Promise<Object[]>} Promise object which resolves with an array of results from the query. Each object in the array corresponds to a row returned from the query. Keys of the object correspond to fields returned, in lowercase.
+ */
+const executeQuery = (queryTemplate, values=[]) => {
+    return new Promise((resolve, reject) => {
+        pool.query(queryTemplate, values).then(res => {
+            resolve(res.rows);
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
 
 // Test code
 executeCommand('INSERT INTO Account (UserName, UserEmail, PassHash) VALUES ($1, $2, $3)', ['Testy McTestFace', 'testyMcT@testington.net', 'abcdefghjik']).then(()=>{
     console.log('Success');
-    process.exit();
+
+    executeQuery('INSERT INTO Account (UserName, UserEmail, PassHash) VALUES ($1, $2, $3) RETURNING UserID', ['Kevin McTestFace', 'kev@gmail.com', 'Capital-P-Assword 1 2 3']).then(rows => {
+        rows.forEach(element => {
+            console.log(element);
+        });
+
+        executeQuery('SELECT NOW() AS now').then(rows => {
+            console.log(rows);
+
+            executeQuery('SELECT PassHash FROM Account WHERE UserName LIKE $1', ['%McTestFace']).then(rows => {
+                rows.forEach(element => {
+                    console.log(element);
+                });
+                process.exit();
+            }).catch(err => {
+                console.error(err);
+            });
+            
+
+        }).catch(err => {
+            console.error();
+        });
+
+    }).catch(err => {
+        console.error(err);
+    });
+
 }).catch(err => {
     console.error(err);
 });
+
+
+
+
+
