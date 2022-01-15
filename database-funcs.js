513c90526debad48a649ddbb2bda0bf794cf5d82
Joe Shepherd <sudo.jshepherd01@gmail.com>
2022-01-15 20:10:17 +0000

    Create simple database function

--- database-funcs.js
+++ database-funcs.js
@@ -2,3 +2,34 @@
  * at a lower level than Pipeline.Store()
  * This is separated to help with securing the database connection
  */
+
+// any credentials that have ever touched GitHub are NOT suitable for a production environment. These need to be abstracted to a different file before this system goes live.
+const db_credentials = {
+    user: 'arresharedev',
+    host: 'localhost',
+    database: 'arresharedev',
+    password: 'SVh}Q,A>3.nL9vp~',
+};
+
+const {Pool} = require('pg');
+
+const pool = new Pool(db_credentials);
+
+const executeCommand = (queryTemplate, values=[]) => {
+    return new Promise((resolve, reject) => {
+        pool.query(queryTemplate, values).then(res => {
+            resolve();
+        }).catch(err => {
+            reject(err);
+        });
+    });
+}
+
+
+// Test code
+executeCommand('INSERT INTO Account (UserName, UserEmail, PassHash) VALUES ($1, $2, $3)', ['Testy McTestFace', 'test@testington.net', 'abcdefghjik']).then(()=>{
+    console.log('Success');
+    process.exit();
+}).catch(err => {
+    console.error(err);
+});
