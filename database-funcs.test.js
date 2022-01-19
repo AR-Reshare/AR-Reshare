9d3dd39d9d9f077474bb2746501cf442dd56b627
Joe Shepherd <sudo.jshepherd01@gmail.com>
2022-01-19 22:09:40 +0000

    Plan database-funcs unit tests

--- database-funcs.test.js
+++ database-funcs.test.js
@@ -19,9 +19,44 @@ describe('Test simple query', () => {
 
     // INSERT affects data as expected
 
+    // Parameterised query works as expected
+
     // DROP is prevented
 
     // SQL injection is prevented
 
     // SELECT on non-existant table rejects
+});
+
+describe('Test complex query', () => {
+    test('Basic SELECT succeeds', () => {
+        let commands = [
+            {
+                text: 'SELECT NOW() AS now',
+            },
+        ];
+
+        return db.complexQuery(commands).then(res => {
+            expect(res).toHaveLength(1);
+            expect(res[0]).toHaveLength(1);
+        });
+    });
+
+    // More tests:
+
+    // Multiple SELECTS return expected data
+
+    // Valid backreference works as expected
+
+    // Paramaterised queries work as expected
+
+    // Invalid backreference rejects
+
+    // Attempting SQL injection using a backreference fails
+
+    // Rejection on initial query works, and additional queries are not sent
+
+    // Rejection mid-way through works, and additional queries are not sent
+
+    // Rejection on final query works, and rollback is sent
 });
