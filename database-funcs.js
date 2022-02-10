35f7642b188effd5389888fcebf66cf25cbc46f5
Joe Shepherd <sudo.jshepherd01@gmail.com>
2022-02-10 10:53:24 +0000

    Extract classes to separate files

--- database-funcs.js
+++
@@ -1,4 +0,0 @@
-/* This file will create the database connection and define the functions used for accessing the database directly
- * at a lower level than Pipeline.Store()
- * This is separated to help with securing the database connection
- */
