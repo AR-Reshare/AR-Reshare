ebbaa2a1cc37f608cc3547b8672ce5a422c0f87a
Joe Shepherd <sudo.jshepherd01@gmail.com>
2022-02-23 23:11:39 +0000

    Implement constructor and status code getter

---
+++ classes/responsetemplate.js
@@ -0,0 +1,144 @@
+const { TemplateError } = require('./errors');
+
+const DefaultErrorMap = {
+    null: 200,
+    AbsentArgumentError: 400,
+};
+
+const isString = (aString) => {
+    return (typeof aString === 'string' || aString instanceof String);
+}
+
+class ResponseTemplate {
+    constructor(params, errorMap={}) {
+        /**
+         * each param has:
+         * - out_name: the name the field will be given in the output
+         * exactly one of:
+         * - field: the name of a field in one row of one query. The value of that field is returned
+         * - rows_with_fields: a field, or array of fields. The value is an array of rows that have all of those fields
+         * - one_row_with_fields: as above, but the value is an object representing the first row that matches
+         */
+
+        this.params = params.map(parameter => {
+            let out = {};
+            
+            // out_name
+            if (!('out_name' in parameter)) {
+                throw new TemplateError('out_name not provided');
+            } else if (!isString(parameter['out_name'])) {
+                throw new TemplateError('out_name not a string');
+            } else if (parameter['out_name'].length === 0) {
+                throw new TemplateError('out_name empty');
+            } else {
+                out.outName = parameter['out_name'];
+            }
+
+            // ensure exactly one of the others is given
+            let numberOfKeys = 0;
+            numberOfKeys += ('field' in parameter);
+            numberOfKeys += ('rows_with_fields' in parameter);
+            numberOfKeys += ('one_row_with_fields' in parameter);
+            if (numberOfKeys !== 1) {
+                throw new TemplateError('Exactly one of field, rows_with_fields, or one_row_with_fields must be specified');
+            }
+
+            // field
+            if (!('field' in parameter)) {
+                out.field = null;
+            } else if (!isString(parameter['field'])) {
+                throw new TemplateError('field not a string');
+            } else if (parameter['field'].length === 0) {
+                throw new TemplateError('field empty');
+            } else {
+                out.field = parameter['field'];
+            }
+
+            // rows_with_fields
+            if (!('rows_with_fields' in parameter)) {
+                out.rowsWithFields = null;
+            } else if (isString(parameter['rows_with_fields'])) {
+                if (parameter['rows_with_fields'].length === 0) {
+                    throw new TemplateError('rows_with_fields is empty');
+                } else {
+                    out.rowsWithFields = [parameter['rows_with_fields']];
+                }
+            } else if (Array.isArray(parameter['rows_with_fields'])) {
+                if (parameter['rows_with_fields'].length === 0) {
+                    throw new TemplateError('rows_with_fields is empty');
+                } else if (parameter['rows_with_fields'].any(field => !isString(field))) {
+                    throw new TemplateError('rows_with_fields contains non-string field name');
+                } else if (parameter['rows_with_fields'].any(field => field.length === 0)) {
+                    throw new TemplateError('rows_with_fields contains empty field name');
+                } else {
+                    out.rowsWithFields = parameter['rows_with_fields'];
+                }
+            } else {
+                throw new TemplateError('rows_with_fields is not string or array');
+            }
+
+            // one_row_with_fields
+            if (!('one_row_with_fields' in parameter)) {
+                out.oneRowWithFields = null;
+            } else if (isString(parameter['one_row_with_fields'])) {
+                if (parameter['one_row_with_fields'].length === 0) {
+                    throw new TemplateError('one_row_with_fields is empty');
+                } else {
+                    out.oneRowWithFields = [parameter['one_row_with_fields']];
+                }
+            } else if (Array.isArray(parameter['one_row_with_fields'])) {
+                if (parameter['one_row_with_fields'].length === 0) {
+                    throw new TemplateError('one_row_with_fields is empty');
+                } else if (parameter['one_row_with_fields'].any(field => !isString(field))) {
+                    throw new TemplateError('one_row_with_fields contains non-string field name');
+                } else if (parameter['one_row_with_fields'].any(field => field.length === 0)) {
+                    throw new TemplateError('one_row_with_fields contains empty field name');
+                } else {
+                    out.oneRowWithFields = parameter['one_row_with_fields'];
+                }
+            } else {
+                throw new TemplateError('one_row_with_fields is not string or array');
+            }
+            
+            return out;
+        });
+
+        /**
+         * errorMap should be a dictionary mapping error classes to status codes
+         * null should map to a success code
+         */
+        if (typeof errorMap !== 'object') {
+            throw new TemplateError('errorMap is not an object');
+        }
+        this.errorMap = errorMap;
+    }
+
+    getStatus(err) {
+        if (err === null) {
+            return this.errorMap[null] || DefaultErrorMap[null];
+        } else if (!(err instanceof Error)) {
+            return 500;
+        } else {
+            return this.errorMap[err.name] || DefaultErrorMap[err.name] || 500;
+        }
+    }
+
+    getResponse(inputArray) {
+        let outputObject = {};
+        this.params.forEach(parameter => {
+            // TODO
+            let value;
+            // search for fields in the inputArray and put the appropriate things in value
+
+            // if field in paramater: get the first occurence of it in the inputArray
+
+            // if rowsWithFields in parameter: find all rows which have those fields
+
+            // if oneRowWithFields in parameter: get the first row which matches
+
+            outputObject[parameter.outName] = value;
+        });
+    }
+}
+
+module.exports = ResponseTemplate;
