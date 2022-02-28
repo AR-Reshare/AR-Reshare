8f4a24d764d52a0588c63b4e7f157157935f70f3
user5423 <davide.correia@outlook.com>
2022-02-28 23:03:30 +0000

    Created a new file for the pushnotifs component

---
+++ classes/pushnotifications.js
@@ -0,0 +1,4 @@
+
+// The AR-Reshare application is meant to be exclusive to the Android platform.
+// We will be using FCM and the corresponding node library to interface with this
+// FCM Docs: https://firebase.google.com/docs/cloud-messaging
