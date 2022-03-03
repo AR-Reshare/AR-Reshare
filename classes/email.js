836f601ba1e4e3da3fa10fc283fe268906ae0467
user5423 <davide.correia@outlook.com>
2022-03-03 11:36:54 +0000

    Created the new email.js file

---
+++ classes/email.js
@@ -0,0 +1,10 @@
+// There are certain events which require the application to send an email, which can include:
+// 1. Account Creation
+// 2. Password Reset
+// 3. Account Detail Modification
+
+// To implement this module, we need to:
+// 1. create the "templates" that we can use to send these emails
+// 2. provide the logic to perform the email sending operations
+// 3. integrate this functionality with the above operations (account-creation, password-reset, account-modification)
+
