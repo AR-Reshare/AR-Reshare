4bfa51c87c47d02afed5d5d0f2fc8baadc9a37d2
user5423 <davide.correia@outlook.com>
2022-03-16 10:32:08 +0000

    Added additional unit tests for replacement object validation

--- tests/unit/unitxx.pushnotifs.test.js
+++ tests/unit/unitxx.pushnotifs.test.js
@@ -25,28 +25,144 @@ describe('Unit Test XX - PushNotif Class Construction', () => {
         }
     });
 
-    test('Valid TemplateType (Message-Start)', () => {
-        let templateType = 'Message-Start';
+    test('Valid TemplateType (MessageStart)', () => {
+        let templateType = 'MessageStart';
         let PushNotifTemplate = new PushNotif(templateType);
         expect(PushNotifTemplate.templateType).toEqual(templateType);
         expect(PushNotifTemplate.templateArguments).toEqual(PushNotifTemplates.arguments[templateType]);
         expect(PushNotifTemplate.templateDict).toEqual(PushNotifTemplates.templates[templateType]);
     });
 
-    test('Valid TemplateType (Message-Send)', () => {
-        let templateType = 'Message-Send';
+    test('Valid TemplateType (MessageSend)', () => {
+        let templateType = 'MessageSend';
         let PushNotifTemplate = new PushNotif(templateType);
         expect(PushNotifTemplate.templateType).toEqual(templateType);
         expect(PushNotifTemplate.templateArguments).toEqual(PushNotifTemplates.arguments[templateType]);
         expect(PushNotifTemplate.templateDict).toEqual(PushNotifTemplates.templates[templateType]);
     });
 
-    test('Valid TemplateType (Message-Close)', () => {
-        let templateType = 'Message-Close';
+    test('Valid TemplateType (MessageClose)', () => {
+        let templateType = 'MessageClose';
         let PushNotifTemplate = new PushNotif(templateType);
         expect(PushNotifTemplate.templateType).toEqual(templateType);
         expect(PushNotifTemplate.templateArguments).toEqual(PushNotifTemplates.arguments[templateType]);
         expect(PushNotifTemplate.templateDict).toEqual(PushNotifTemplates.templates[templateType]);
+    });
+
+});
+
+
+let pushNotifTemplate, templateType, fcmApp;
+
+
+beforeAll(async () => {
+    fcmApp = await PushNotifHelper.initializeApp();
+});
+
+beforeEach(() => {
+    templateType = 'MessageSend';
+    pushNotifTemplate = new PushNotif(templateType);
+});
+
+describe('Unit Test XX - PushNotif Class (template replacement validate)', () => {
+    test('Missing replacementObject', async () => {
+        let inputObject = null;
+        let registrationToken = null;
+        let expectedError = new AbsentArgumentError();
+
+        let out = pushNotifTemplate.process(fcmApp, registrationToken, inputObject);
+        expect(out).rejects.toEqual(expectedError);
+    });
+
+    test('Overexceeded replacement argument length', () => {
+        let inputObject = {
+            senderName: 'Sam Sepiol',
+            senderMessage: 'Hi, I\'m interested about the laptop for sale',
+            exceededVariable: 'This shouldn\'t be processed'
+        };
+        let registrationToken = null;
+        let expectedError = new InvalidArgumentError();
+
+        let out = pushNotifTemplate.process(fcmApp, registrationToken, inputObject);
+        expect(out).rejects.toEqual(expectedError);
+    });
+
+    
+    test('Insufficient replacement argument length', () => {
+        let inputObject = {
+            senderName: 'Sam Sepiol'
+        };
+
+        let registrationToken = null;
+        let expectedError = new InvalidArgumentError();
+
+        let out = pushNotifTemplate.process(fcmApp, registrationToken, inputObject);
+        expect(out).rejects.toEqual(expectedError);
+    });
+    
+
+    test('Correct replacement argument length + included invalid attribute', () => {
+        let inputObject = {
+            senderName: 'Sam Sepiol',
+            randomAttribute: 'randomElement'
+        };
+
+        let registrationToken = null;
+        let expectedError = new InvalidArgumentError();
+
+        let out = pushNotifTemplate.process(fcmApp, registrationToken, inputObject);
+        expect(out).rejects.toEqual(expectedError);
+    });
+
+    
+    test('Correct replacement argument length + all invalid attribute', () => {
+        let inputObject = {
+            randomAttribute1: 'foo@example.com',
+            randomAttribute2: 'randomElement'
+        };
+
+        let registrationToken = null;
+        let expectedError = new InvalidArgumentError();
+
+        let out = pushNotifTemplate.process(fcmApp, registrationToken, inputObject);
+        expect(out).rejects.toEqual(expectedError);
+    });
+    
+    test('Correct replacement argument length + Correct attributes + Incorrect value type', () => {
+        let inputObject = {
+            senderName: 5,
+            senderMessage: 'randomElement'
+        };
+
+        let registrationToken = null;
+        let expectedError = new InvalidArgumentError();
+
+        let out = pushNotifTemplate.process(fcmApp, registrationToken, inputObject);
+        // let out = pushNotifTemplate.replacementObjectValidate(inputObject);
+        expect(out).rejects.toEqual(expectedError);
+    });
+
+    test('Correct replacement argument length + Correct attributes + Incorrect values\' types', () => {
+        let inputObject = {
+            senderName: null,
+            randomAttribute2: 5
+        };
+
+        let registrationToken = null;
+        let expectedError = new InvalidArgumentError();
+
+        let out = pushNotifTemplate.process(fcmApp, registrationToken, inputObject);
+        expect(out).rejects.toEqual(expectedError);
+    });
+
+    test('Valid replacementObject', () => {
+        let inputObject = {
+            senderName: 'Sam Sepiol',
+            senderMessage: 'Test message'
+        };
+
+        let out = pushNotifTemplate.replacementObjectValidate(inputObject);
+        expect(out).resolves.toEqual(true);
     });
 
 });
