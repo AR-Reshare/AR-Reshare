5a39d07cf8ead304a6677e030d5525529cf2de60
user5423 <davide.correia@outlook.com>
2022-03-19 10:26:16 +0000

    Added empty test file for pushnotifhelper class

---
+++ tests/unit/unitxx.pushnotifhelper.test.js
@@ -0,0 +1,2 @@
+const {PushNotif, PushNotifTemplates, PushNotifHelper} = require('../../classes/pushnotifications.js');
+const {TemplateError, InvalidArgumentError, AbsentArgumentError} = require('../../classes/errors.js');
